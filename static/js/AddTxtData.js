2e74ed632e21e8a4bedb0a3efa932ca6918e0ebd
BeiFeng <2798305063@qq.com>
2023-08-26 16:33:30 +0800

    update前端

--- static/js/AddTxtData.js
+++ static/js/AddTxtData.js
@@ -1,8 +1,10 @@
+var loading = document.getElementById('loading');
+loading.style.visibility = 'hidden';
 $(document).ready(function () {
-
     //数据上传
     var fileInput = document.getElementById('fileInput');
     fileInput.addEventListener('change', function(event) {
+        loading.style.visibility = 'visible';
         var fileName = fileInput.value.split('\\').pop();
         var file = event.target.files[0];
         var reader = new FileReader();
@@ -19,13 +21,16 @@ $(document).ready(function () {
                 success: function(response) {
                     if(response.status == 'failure'){
                         swal("数据上传失败!", response.message, "error");
+                        loading.style.visibility = 'hidden';
                     }
                     else{
                         swal("数据上传成功", response.message, "success");
+                        loading.style.visibility = 'hidden';
                     }
                 },
                 error: function() {
                     swal("request failed!", "ajax request failed!", "error");
+                    loading.style.visibility = 'hidden';
                 }
             });
         };
